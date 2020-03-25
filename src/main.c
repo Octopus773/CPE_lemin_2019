14a1209cec5ede7ad4593391036cce4fb498bf52
arthur <arthur.jamet@epitech.eu>
2020-03-25 11:27:30 +0100

    correction of main structure for error handling

--- src/main.c
+++ src/main.c
@@ -11,12 +11,14 @@
 int main(void)
 {
     lemin_t *lemin = get_lemin();
+    ant_t *ants = NULL;
+    rooms_t *end_room = NULL;
+
     if (!lemin)
         return (ERROR);
-    ant_t *ants = get_ants(lemin);
-    rooms_t *end_room = get_end_room(lemin);
-
-    if (!lemin || !ants || ! end_room)
+    ants = get_ants(lemin);
+    end_room = get_end_room(lemin);
+    if (!ants || !end_room)
         return (ERROR);
     my_printf("#moves\n");
     while (play_a_turn(ants, end_room));
