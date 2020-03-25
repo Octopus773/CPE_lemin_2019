1336d8e63a7ca549e95c45780c72bd88eee96921
Anonymus Raccoon <tr.tristan-roux@outlook.fr>
2020-03-25 14:18:35 +0100

    Making the algorithm work

--- src/pathfinding/pathfinding.c
+++ src/pathfinding/pathfinding.c
@@ -16,46 +16,52 @@ void print_links(rooms_t *room)
     ENDFOREACH(i, room->links)
 }
 
-rooms_t *proceed_room(rooms_t *room, rooms_t *prev_room)
+path_t proceed_room(rooms_t *room, rooms_t *prev_room)
 {
-    rooms_t *best_room = NULL;
-    rooms_t *tmp;
+    path_t best_path = {.room = NULL, .score = 999};
+    path_t tmp;
     //my_printf("\nstart room %s\n", room->name);
     //print_links(room);
 
     if (prev_room)
-        room->score = prev_room->score + (room->occupied) ? 2 : 1;
+        room->score = prev_room->score + (room->occupied ? 2 : 1);
+    else
+        room->score = 0;
+    //my_printf("Room score %s: %d\n", room->name, room->score);
     if (room->type == END) {
-       // my_printf("END SCORE: %i\n", room->score);
-        return (room);
+        //my_printf("END SCORE: %i\n", room->score);
+        best_path.score = room->score;
+        best_path.room = room;
+        return (best_path);
     }
+    room->is_proceeded = true;
     FOREACH(rooms_t, r_linked, i, room->links)
-        //my_printf("check r %s, occupied %i, proceed %i\n", r_linked->name, r_linked->occupied, r_linked->is_proceeded);
-        if (r_linked->is_proceeded || r_linked->type == START) {
-          //  my_printf("skip\n");
+        if (r_linked->is_proceeded) {
             CONTINUE(i);
         }
         r_linked->is_proceeded = true;
-       // my_printf("proceed room link %s\n", r_linked->name);
         tmp = proceed_room(r_linked, room);
-     //   my_printf("got room %s ret from %s score tmp: %i best score : %i\n", tmp->name, r_linked->name, tmp->score, best_room ? best_room->score : 999);
+        //my_printf("Curent: %s, Best: %d -- Foreach room: %s - score: %d\n", room->name, best_path.score, tmp.room->name, tmp.score);
         r_linked->is_proceeded = false;
-        r_linked->score += tmp->score;
-        if (!best_room || tmp->score < best_room->score)
-            best_room = r_linked;
+        if (!best_path.room || tmp.score <= best_path.score) {
+            best_path.room = r_linked;
+            best_path.score = tmp.score;
+            // my_printf("Changing %s's score: %d\n", best_path.room->name, best_room->score);
+        }
     ENDFOREACH(i, room->links)
-    if (!best_room) {
-        best_room = room;
-        best_room->score = 999999999;
+    room->is_proceeded = false;
+    if (!best_path.room) {
+        best_path.room = room;
+        best_path.score = 999999999;
     }
-   // my_printf("Best room score: %i\n", best_room->score);
-    return (best_room);
+   // my_printf("Best room score: %i\n", best_path.score);
+    return (best_path);
 }
 
 rooms_t *get_best_room(ant_t *ant)
 {
     if (!ant)
         return (NULL);
-    //my_printf("\n\nfor ant %i\n", ant->id);
-    return (proceed_room(ant->current_room, ant->old_room));
+   // my_printf("\n\nfor ant %i (current room: %s)\n", ant->id, ant->current_room->name);
+    return (proceed_room(ant->current_room, NULL).room);
 }
